681130b3b10a2130537e7624d73275c618e11c6e
PatWil <plwg123@outlook.com>
2021-06-25 06:39:48 -0700

    alta3 at number 12 list of list

--- first.py
+++ first.py
@@ -9,3 +9,20 @@ print(dd);
 
 arr=[1,2,3]
 print(arr)
+print(arr[2])
+
+# arr.extend(5) cant be ints cant iterate unless and list
+arr.extend("abc")
+print(arr)
+
+proto2 = [ 22, 80, 443, 53 ]
+
+arr.append(proto2)
+
+print(arr)
+
+arr2 = arr
+
+arr2.extend(proto2)
+
+print(arr2)
